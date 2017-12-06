dc23293026b9b5bdf1552f103575867bfc8951cb
mazurjestboski <mazurjestboski@mazurjestboskis-MacBook-Pro.local>
2017-12-06 02:05:13 +0100

    working photos and colour background

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function getAllPaintings(){
     .then(showPaintings)
 }
 
-
+var colours = [];
 function showPaintings(data){
 let list = document.querySelector("#list");
 let template = document.querySelector("#paintingTemplate").content;
@@ -18,19 +18,26 @@ let clone = template.cloneNode(true);
    // popImage.style.backgroundImage = thePainting._embedded["wp:featuredmedia"]["0"].media_details.sizes.large.source_url;
 
 
-
+let counter = 0;
 data.forEach(function(thePainting){
     console.log(thePainting);
     let clone = template.cloneNode(true);
-    let img = clone.querySelector("#popupImg img");
+    let popImage = clone.querySelector(".popupImg");
+    let img = clone.querySelector(".popupImg img");
+    let titleDiv = clone.querySelector("#paintingTitle");
     let title = clone.querySelector("#paintingTitle #t1");
     let date = clone.querySelector("#paintingTitle #t2");
-    let popImage = clone.querySelector("#popupImg");
     let photo = thePainting._embedded["wp:featuredmedia"]["0"].media_details.sizes.large.source_url;
+    colours.push(thePainting.acf.background_colour);
     img.setAttribute("src", photo);
+    popImage.setAttribute("id", "lol"+counter);
     title.textContent= thePainting.title.rendered;
+    titleDiv.setAttribute("class", counter);
     date.textContent= thePainting.acf.date;
+    counter = counter + 1;
+    console.log(counter);
     list.appendChild(clone);
+
 })
 
 list.appendChild(clone);
@@ -39,11 +46,16 @@ list.appendChild(clone);
 getAllPaintings();
 
 
-function popImg() {
-    document.getElementById("popupImg").style.width = "50%";
+function popImg(obj) {
+    let element = document.getElementById("lol"+obj.className);
+    let bg = document.getElementById("background");
+    console.log(bg);
+    element.style.width="50%";
+    bg.style.backgroundColor= colours[obj.className];
 }
-function hideImg() {
-    document.getElementById("popupImg").style.width = "0";
+function hideImg(obj) {
+    let element = document.getElementById("lol"+obj.className);
+    element.style.width="0";
 }
 
 /////////OVERLAY MENU////////////
