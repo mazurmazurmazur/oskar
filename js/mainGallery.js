f01181317c5ffa8f4a772240ea33d72228eba2db
mazurjestboski <mazurjestboski@mazurjestboskis-MacBook-Pro.local>
2017-12-14 23:08:54 +0100

    first gallery sorting

--- js/mainGallery.js
+++ js/mainGallery.js
@@ -1,11 +1,21 @@
 var paintings = [];
+var years = [];
+ let a2020;
+ let a2019;
+ let a2018;
+ let a2017;
+ let a2016;
+ let a2014;
+ let a2013;
+
 function getAllPaintings() {
-  fetch("http://mazurmazurmazur.com/wordpress/wp-json/wp/v2/paintings?_embed")
+  fetch("http://oskar.sharemedia.dk/wp-json/wp/v2/works?_embed&per_page=50")
     .then(res => res.json())
     .then(showPaintings)
 }
 
 function showPaintings(data) {
+    console.log(data);
     let list = document.querySelector("#wrapper");
     let template = document.querySelector("#paintingTemplate").content;
     let clone = template.cloneNode(true);
@@ -13,8 +23,10 @@ function showPaintings(data) {
         let clone = template.cloneNode(true);
             let imgR = clone.querySelector("#rightImg");
             let imgL = clone.querySelector("#leftImg")
-            let photo = thePainting._embedded["wp:featuredmedia"]["0"].media_details.sizes.large.source_url;
+            let photo = thePainting._embedded["wp:featuredmedia"]["0"].media_details.sizes.full.source_url;
+            let year = thePainting.acf.year;
             paintings.push(photo);
+            years.push(year);
             list.appendChild(clone);
     })
         fillColumns();
@@ -30,17 +42,52 @@ getAllPaintings();
         for(i=0; i<paintings.length; i=i+2){
             var img = document.createElement("img");
             img.src = paintings[i];
+            img.setAttribute("class", "a"+years[i]);
             document.querySelector("#leftImg").appendChild(img);
         }
 
                for(i=1; i<paintings.length; i=i+2){
             var img = document.createElement("img");
             img.src = paintings[i];
+            img.setAttribute("class", "a"+years[i]);
             document.querySelector("#rightImg").appendChild(img);
         }
-
+            a2020 = document.querySelectorAll(".a2020");
+            a2019 = document.querySelectorAll(".a2019");
+            a2018 = document.querySelectorAll(".a2018");
+            a2017 = document.querySelectorAll(".a2017");
+            a2016 = document.querySelectorAll(".a2016");
+            a2014 = document.querySelectorAll(".a2014");
+            a2013 = document.querySelectorAll(".a2013");
 
     }
+
+function hideOtherYears(year){
+
+
+    for(i = 0; i < a2020.length; i++) {
+    a2020[i].style.display = "none";
+}
+    for(i = 0; i < a2019.length; i++) {
+    a2019[i].style.display = "none";
+}
+     for(i = 0; i < a2018.length; i++) {
+    a2018[i].style.display = "none";
+}
+     for(i = 0; i < a2017.length; i++) {
+    a2017[i].style.display = "none";
+}
+     for(i = 0; i < a2016.length; i++) {
+    a2016[i].style.display = "none";
+}
+     for(i = 0; i < a2014.length; i++) {
+    a2014[i].style.display = "none";
+}
+     for(i = 0; i < a2013.length; i++) {
+    a2013[i].style.display = "none";
+}
+
+}
 
 
 /////////OVERLAY MENU////////////
